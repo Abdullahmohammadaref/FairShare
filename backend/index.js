a6ade69607f7e8f7cba8520637bc122a6d28e3fd
Abdullahmohammadaref <abdullahmohammadaref@gmail.com>
2026-02-22 06:09:43 +0300

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors';
+import { createClient } from '@supabase/supabase-js'
+import "dotenv/config";
+
+
+
+
+const app = express()
+app.use(cors());
+app.use(express.json())
+
+// Create a single supabase client for interacting with your database
+createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
+
+
+
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000')
+})
+
+
+
